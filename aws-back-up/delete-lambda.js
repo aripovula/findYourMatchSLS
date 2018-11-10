6035526a8033d675de6f0fa7aea46464f674b5bc
Ulugbek Aripov <ulugbeks@gmail.com>
2018-11-10 12:42:47 -0500

    added code in front end and in AWS Lambda and API Gateway to dynamicly delete data from DynamoDB - request uses token to protect unauthorized delete

--- aws-back-up/delete-lambda.js
+++ aws-back-up/delete-lambda.js
@@ -1,14 +1,15 @@
 const AWS = require('aws-sdk');
 const dynamodb = new AWS.DynamoDB({ region: 'us-east-2', apiVersion: '2012-08-10' });
-const cisp = new AWS.CognitoIndentityServiceProvider({ version: '2016-04-18' });
+const cisp = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
 
 exports.handler = (event, context, callback) => {
     const accessToken = event.accessToken;
     const candidateID = event.candidateID;
+    console.log('candidateID = ', candidateID);
     const params = {
         Key: {
             "UserID": {
-                S: "a01"
+                S: candidateID
             }
         },
         TableName: 'find-your-match'
@@ -16,11 +17,14 @@ exports.handler = (event, context, callback) => {
     const cispParams = {
         "AccessToken": accessToken
     };
-    cispParams.getUser(cispParams, (err, result) => {
+    cisp.getUser(cispParams, (err, result) => {
         if (err) {
             console.log(err);
             callback(err);
         } else {
+            // I don't use user ID here - I want to make sure that passed token is valid to
+            // prevent someone else deleting anything in Dynamo DB.
+            
             dynamodb.deleteItem(params, function (err, data) {
                 if (err) {
                     console.log(err);
@@ -34,6 +38,5 @@ exports.handler = (event, context, callback) => {
         }
 
     });
-
 
 };
