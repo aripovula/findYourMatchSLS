3515b17c5221b31285689e3658cb1a037e2d8ab5
Ulugbek Aripov <ulugbeks@gmail.com>
2018-12-05 12:04:33 -0500

    added Image Capturing from PC Webcam using NPM

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { WebcamModule } from 'ngx-webcam';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -14,10 +16,12 @@ import { CognitoService } from './services/cognito.service';
 import { FindMatchFormComponent } from './find-match-form/find-match-form.component';
 import { AuthGuard } from './services/auth-guard.service';
 import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
 
 const appRoutes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'home', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: 'start', component: StartComponent, canActivate: [AuthGuard] },
   { path: 'all_users_admin_only', component: AllUsersComponent, canActivate: [AuthGuard] },
 
   { path: 'not-found', component: NotFoundComponent },
@@ -33,6 +37,7 @@ const appRoutes: Routes = [
     AllUsersComponent,
     FindMatchFormComponent,
     HomeComponent,
+    StartComponent,
   ],
   imports: [
     BrowserModule,
@@ -41,7 +46,10 @@ const appRoutes: Routes = [
     RouterModule.forRoot(
       appRoutes
     ),
-    Ng2SmartTableModule
+    Ng2SmartTableModule,
+    WebcamModule,
+    FormsModule,
+
   ],
   providers: [CognitoService, DataService, AuthGuard],
   bootstrap: [AppComponent]
